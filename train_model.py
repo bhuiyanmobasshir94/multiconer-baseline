c1d1d910631bff674002b3778edf333d0d69cbf9
bhuiyanmobasshir94 <mobasshirbhuiyan.shagor@gmail.com>
2021-12-09 14:58:23 +0600

    Changes on train_model

--- train_model.py
+++ train_model.py
@@ -1,6 +1,11 @@
 import time
+import warnings
 
-from utils.utils import get_reader, train_model, create_model, save_model, parse_args, get_tagset
+from utils.utils import (create_model, get_reader, get_tagset, parse_args,
+                         save_model, train_model)
+
+warnings.filterwarnings("ignore")
+
 
 if __name__ == '__main__':
     timestamp = time.time()
@@ -8,8 +13,10 @@ if __name__ == '__main__':
     out_dir_path = sg.out_dir + '/' + sg.model_name
 
     # load the dataset first
-    train_data = get_reader(file_path=sg.train, target_vocab=get_tagset(sg.iob_tagging), encoder_model=sg.encoder_model, max_instances=sg.max_instances, max_length=sg.max_length)
-    dev_data = get_reader(file_path=sg.dev, target_vocab=get_tagset(sg.iob_tagging), encoder_model=sg.encoder_model, max_instances=sg.max_instances, max_length=sg.max_length)
+    train_data = get_reader(file_path=sg.train, target_vocab=get_tagset(
+        sg.iob_tagging), encoder_model=sg.encoder_model, max_instances=sg.max_instances, max_length=sg.max_length)
+    dev_data = get_reader(file_path=sg.dev, target_vocab=get_tagset(
+        sg.iob_tagging), encoder_model=sg.encoder_model, max_instances=sg.max_instances, max_length=sg.max_length)
 
     model = create_model(train_data=train_data, dev_data=dev_data, tag_to_id=train_data.get_target_vocab(),
                          dropout_rate=sg.dropout, batch_size=sg.batch_size, stage=sg.stage, lr=sg.lr,
@@ -18,4 +25,5 @@ if __name__ == '__main__':
     trainer = train_model(model=model, out_dir=out_dir_path, epochs=sg.epochs)
 
     # use pytorch lightnings saver here.
-    out_model_path = save_model(trainer=trainer, out_dir=out_dir_path, model_name=sg.model_name, timestamp=timestamp)
+    out_model_path = save_model(
+        trainer=trainer, out_dir=out_dir_path, model_name=sg.model_name, timestamp=timestamp)
